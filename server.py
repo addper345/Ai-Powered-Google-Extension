b6d2266108a74d8b70cd848af7a0451bdcf6dea8
Aden P <addper345@gmail.com>
2025-08-01 00:16:01 -0400

    Completed server setup

--- server.py
+++ server.py
@@ -5,15 +5,19 @@ app = Flask(__name__)
 
 @app.route("/upload", methods=['POST'])
 def send():
-    if request.method == 'POST':
-        count = 0
-        with open('number.txt', 'r') as file:
-            content = file.read()
-            count = int(content)
-            count+=1
+    count = 0
+    print(request.files)
+    with open('number.txt', 'r') as file:
+        content = file.read()
+        count = int(content)
+        count+=1
 
-        with open('number.txt', 'w') as file:
-            file.write(count)
-        
-        f = request.files['the_file']
-        f.save(f'files/{count}.txt')
+    with open('number.txt', 'w') as file:
+        file.write(count)
+    
+    f = request.files
+    f.save(f'files/{count}.txt')
+
+@app.route('/')
+def home():
+    return 'hi'
